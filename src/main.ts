662088fe328b2e2ba2c19355dd7e29c49185fab7
leemhoon00 <leemhoon000@gmail.com>
2024-09-11 18:19:58 +0900

    init

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  });
+
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('PetMily API')
+    .setDescription('PetMily API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addTag('PetMily')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+  console.log(`listening on port 3000`);
+}
+bootstrap();
