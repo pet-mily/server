6d30aa53ff789358fd7842af063ff9bdd35fb899
leemhoon00 <leemhoon000@gmail.com>
2024-09-13 01:40:02 +0900

    feat: upload pet image

---
+++ src/aws/aws.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AwsService {
+  constructor(
+    private configService: ConfigService,
+    private readonly s3Client: S3Client,
+  ) {}
+
+  async uploadPetImage(petId: string, image: Express.Multer.File) {
+    const command = new PutObjectCommand({
+      Bucket: this.configService.get('AWS_BUCKET_NAME'),
+      Key: `pets/${petId}.${image.filename.split('.').pop()}`,
+      Body: image.buffer,
+      ContentType: image.mimetype,
+    });
+
+    return this.s3Client.send(command);
+  }
+}
